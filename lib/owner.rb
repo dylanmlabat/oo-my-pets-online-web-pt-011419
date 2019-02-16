dc14e39e93ea50cad71e517244ae590e78ce03ea
Dylan Michael Labat <dml252@live.com>
2019-02-16 13:55:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,16 @@
 class Owner
   attr_accessor :name
   attr_reader :species
+  @@all = []
   
   def initialize(name, species = "human")
     @name = name
     @species = species
     @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
