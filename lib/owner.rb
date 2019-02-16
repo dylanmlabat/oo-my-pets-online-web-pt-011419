5c85e03ad42b52e92720519af1f9aaf1ada4972d
Dylan Michael Labat <dml252@live.com>
2019-02-16 13:56:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,21 +2,15 @@ class Owner
   attr_accessor :name
   attr_reader :species
   @@all = []
-  owner_count = 0
   
   def initialize(name, species = "human")
     @name = name
     @species = species
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
-    owner_count += 1
   end
   
   def self.all
     @@all
-  end
-  
-  def self.count
-    owner_count
   end
 end
