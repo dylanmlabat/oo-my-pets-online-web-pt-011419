15de8d754c6f4a68a2a8238b098ce134e4c63ee7
Dylan Michael Labat <dml252@live.com>
2019-02-16 13:53:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,7 @@ class Owner
   attr_accessor :name
   attr_reader :species
   
-  def initialize(name, species)
+  def initialize(name, species = nil)
     @name = name
     @species = species
   end
