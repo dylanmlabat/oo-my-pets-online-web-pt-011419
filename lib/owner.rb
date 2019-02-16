b1b6582dc5e9cd7955c5d2c1d020cc29360ad083
Dylan Michael Labat <dml252@live.com>
2019-02-16 14:12:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,6 +61,6 @@ class Owner
   end
   
   def list_pets
-    "I have #{pets[:fishes].count} fish, #{pets[:fishes].count} dog(s), and  cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 end
